b3b5752ceb2d55032c90f3aa51e0fbc6b1893b6d
Tarun Hawdia <tarunhawdiya112@gmail.com>
2025-06-02 16:15:39 +0530

    added new

---
+++ A_Square_Year.cpp
@@ -0,0 +1,195 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <array>
+#include <deque>
+#include <list>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <tuple>
+#include <utility>
+#include <algorithm>
+#include <numeric>    // iota, accumulate, gcd, lcm
+#include <functional> // function, bind
+
+// Math and limits
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <cfloat>
+#include <cassert>
+#include <ctime>
+// Advanced types
+#include <complex>
+#include <type_traits>
+#include <random>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <ios>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define vll vector<ll>
+#define vi vector<int>
+
+// Debugging
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Tarun cout.tie(NULL);
+
+void solve()
+{
+    string s;
+    cin >> s;
+    int num = stoi(s);
+    bool found = false;
+
+    int root = sqrt(num);
+    if (root * root != num)
+    {
+        cout << -1 << "\n";
+        return;
+    }
+
+    // Try all possible a + b = root
+    for (int a = 0; a <= root; ++a)
+    {
+        int b = root - a;
+        if (a >= 0 && b >= 0 && a <= 99 && b <= 99)
+        {
+            cout << a << " " << b << "\n";
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+        cout << -1 << "\n";
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    Code By Tarun
+
+        ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
