7feacf3007d7d1a6a397dfca6924a2b48bd6c03d
Tarun Hawdia <111425737+Tarunhawdia@users.noreply.github.com>
2026-02-24 10:59:51 +0530

    Auto-Sync Codeforces 71A - Way-Too-Long-Words

--- Codeforces/71/A_Way-Too-Long-Words.cpp
+++ Codeforces/71/A_Way-Too-Long-Words.cpp
@@ -1,9 +1,9 @@
 // Problem: A - Way-Too-Long-Words
 // Contest: 71
-// Language: C++23 (GCC 14-64, msys2)
+// Language: C++17 (GCC 7-32)
 // Tags: strings, *800
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 #define MOD 1000000007
@@ -20,6 +20,7 @@ using namespace std;
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 
+
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double lld;
@@ -27,58 +28,56 @@ typedef long double lld;
 #define vll vector<ll>
 #define vi vector<int>
 
-// Debugging
+
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
 #ifndef ONLINE_JUDGE
 #define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
 #else
 #define debug(x)
 #endif
 
-void _print(ll t) { cerr << t; }
-void _print(int t) { cerr << t; }
-void _print(string t) { cerr << t; }
-void _print(char t) { cerr << t; }
-void _print(lld t) { cerr << t; }
-void _print(double t) { cerr << t; }
-void _print(ull t) { cerr << t; }
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
 
-template <class T, class V> void _print(pair<T, V> p);
-template <class T> void _print(vector<T> v);
-template <class T> void _print(set<T> v);
-template <class T, class V> void _print(map<T, V> v);
-template <class T> void _print(multiset<T> v);
-template <class T, class V> void _print(pair<T, V> p) {
-    cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";
-}
-template <class T> void _print(vector<T> v) {
-    cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]";
-}
-template <class T> void _print(set<T> v) {
-    cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]";
-}
-template <class T> void _print(multiset<T> v) {
-    cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]";
-}
-template <class T, class V> void _print(map<T, V> v) {
-    cerr << "[ "; for (auto i : v) { _print(i); cerr << " "; } cerr << "]";
-}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
 
 #define Code ios_base::sync_with_stdio(false);
 #define By cin.tie(NULL);
 #define Tarun cout.tie(NULL);
 
+const ll mod = 1e9 + 7;
+
 void solve() {
     string s;
-    cin>>s;
+    cin >> s;
 
-    int n=s.size();
-    if(n<=10){
-        cout<<s<<nline;
+    ll n = s.size();
+
+    if (n <= 10) {
+        cout << s << nline;
     }
-    else{
-        cout<<s[0];
-        cout<<n-2;
-        cout<<s[n-1]<<nline;
+    else {
+        cout << s[0];
+        cout << n - 2;
+        cout << s[n - 1];
+        cout << nline;
     }
 
 }
@@ -86,7 +85,7 @@ void solve() {
 int main() {
 
 #ifndef ONLINE_JUDGE
-    freopen("Error.txt", "w", stderr);
+    freopen("Error.txt", "w" , stderr);
 #endif
 
     Code By Tarun
@@ -99,3 +98,5 @@ int main() {
 
     return 0;
 }
+
+
