1c45f7ae2f52eb89e705ecb51078eac4f722f65b
Tarun Hawdia <111425737+Tarunhawdia@users.noreply.github.com>
2026-02-24 11:00:09 +0530

    Auto-Sync Codeforces 4A - Watermelon

--- Codeforces/4/A_Watermelon.cpp
+++ Codeforces/4/A_Watermelon.cpp
@@ -1,19 +1,79 @@
 // Problem: A - Watermelon
 // Contest: 4
-// Language: C++20 (GCC 13-64)
+// Language: C++17 (GCC 7-32)
 // Tags: brute force, math, *800
 
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define vll vector<ll>
+#define vi vector<int>
+
+
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Tarun cout.tie(NULL);
+
+const ll mod = 1e9 + 7;
+void solve(){
     int w;
     cin >> w;
-
-    if (w > 2 && w % 2 == 0)
-        cout << "YES";
-    else
-        cout << "NO";
-
-    return 0;
+    if (w % 2 == 0 && w > 2){
+        cout << "YES" << nline;
+    }
+    else{
+        cout << "NO" << nline;
+    }
+}
+int main(){
+    solve();
+    
 }
